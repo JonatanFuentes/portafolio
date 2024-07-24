d9d1cac03daaaadcd4996eadd0d149c51ec3c50b
Jonatan Fuentes Cerezo <146351318+JonatanFuentes@users.noreply.github.com>
2024-07-24 01:46:01 -0400

    Update main4.js

--- main4.js
+++ main4.js
@@ -7,9 +7,9 @@ function initModel(containerId, modelPath) {
     const width = container.clientWidth;
     const height = container.clientHeight;
 
-    // Crear la escena, la cámara y el renderizador
+  
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000); // Ajusta el FOV
+    const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000); 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.outputColorSpace = THREE.SRGBColorSpace;
     renderer.setSize(width, height);
@@ -17,37 +17,36 @@ function initModel(containerId, modelPath) {
     renderer.setPixelRatio(window.devicePixelRatio);
     container.appendChild(renderer.domElement);
 
-    // Crear y agregar luces
+  
     const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
     spotLight.position.set(25, 25, 25);
     spotLight.castShadow = true;
     spotLight.shadow.bias = -0.0001;
     scene.add(spotLight);
 
-    // Crear y agregar el suelo
- // Crear y agregar el suelo circular
- const radius = 2; // Radio del suelo circular
- const groundGeometry = new THREE.CircleGeometry(radius, 25); // 32 segmentos para un suelo más suave
+    
+ const radius = 2; 
+ const groundGeometry = new THREE.CircleGeometry(radius, 25);
  const groundMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
  const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
- groundMesh.rotation.x = -Math.PI / 2; // lados
- groundMesh.position.y = -1.3; // arriba abajo
+ groundMesh.rotation.x = -Math.PI / 2; 
+ groundMesh.position.y = -1.3;
 
  scene.add(groundMesh);
 
-    // Configurar controles
+   
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.enablePan = true;
-    controls.minDistance = 2;  // Ajusta según el tamaño de tu modelo
-    controls.maxDistance = 10; // Ajusta según el tamaño de tu modelo
-    controls.minPolarAngle = 0; // Ángulo mínimo para rotar hacia abajo
-    controls.maxPolarAngle = Math.PI / 2; // Ángulo máximo para rotar hacia arriba
+    controls.minDistance = 2;  
+    controls.maxDistance = 10; 
+    controls.minPolarAngle = 0; 
+    controls.maxPolarAngle = Math.PI / 2; 
     controls.autoRotate = false;
     controls.target.set(0, 1, 0);
     controls.update();
 
-    // Cargar el modelo
+ 
     const loader = new GLTFLoader().setPath('millennium_falcon/');
     loader.load(modelPath, (gltf) => {
         console.log('loading model');
@@ -56,15 +55,15 @@ function initModel(containerId, modelPath) {
         mesh.traverse((child) => {
             if (child.isMesh) {
                 child.castShadow = true;
-                child.receiveShadow = false; // Desactivar la recepción de sombras
+                child.receiveShadow = false; 
             }
         });
 
         mesh.position.set(0, 0, 0);
-        mesh.scale.set(3, 3, 3); // Ajusta la escala del modelo si es necesario
+        mesh.scale.set(3, 3, 3); 
         scene.add(mesh);
 
-        // Ajustar la cámara para que el modelo sea visible
+     
         const boundingBox = new THREE.Box3().setFromObject(mesh);
         const center = new THREE.Vector3();
         boundingBox.getCenter(center);
@@ -78,14 +77,14 @@ function initModel(containerId, modelPath) {
         console.error(error);
     });
 
-    // Animación
+    
     function animate() {
         requestAnimationFrame(animate);
-        controls.update(); // Actualiza los controles en cada frame
+        controls.update(); 
         renderer.render(scene, camera);
     }
 
-    // Ajustar el tamaño del renderer al tamaño del contenedor
+    
     function resizeRenderer() {
         const width = container.clientWidth;
         const height = container.clientHeight;
